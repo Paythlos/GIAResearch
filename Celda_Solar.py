03817a39aa581a8ae2f04230ba9727a0213af960
Alex <45118061+Paythlos@users.noreply.github.com>
2021-03-02 01:05:55 -0500

    Test
    Cambiando datos

--- Celda_Solar.py
+++ Celda_Solar.py
@@ -13,12 +13,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 #Region heterounion pn, unidades en cm
-L = 3e-4    #Longitud de la celda
-x = np.concatenate((np.linspace(0,1.2e-4,100,endpoint=False),(np.linspace(1.2e-4, L, 50))))
+L = 1e-4    #Longitud de la celda
+x = np.concatenate((np.linspace(0,0.2e-4,100,endpoint=False),(np.linspace(0.2e-4, L, 50))))
 
 #Creacion de la malla con Sesame
 sys = sesame.Builder(x)
 
+####################################################################################################
 #Detalles del material
 material = {'Nc':8e17, 'Nv':1.8e19, 'Eg':1.5, 'affinity':3.9, 'epsilon':9.4,
         'mu_e':100, 'mu_h':100, 'Et':0, 'tau_e':10e-9, 'tau_h':10e-9, 'Et':0}
@@ -47,10 +48,11 @@ sys.add_acceptor(nA, p_region)  #Adiciona los aceptadores a la region del semico
 # Definicion de contactos Ohmicos
 sys.contact_type('Ohmic', 'Ohmic')
 
-#Definicion de tasas de recombinacion electron-hueco en la superficie
+#Definicion de tasas de recombinacion electron-hueco en la superficie, haciendo la homounion pn
 Sn_left, Sp_left, Sn_right, Sp_right = 1e7, 0, 0, 1e7  # cm/s
 sys.contact_S(Sn_left, Sp_left, Sn_right, Sp_right)
 
+######################################################################################################
 #Definicion de elementos luminicos
 phi = 1e17       # flujo de fotones [1/(cm^2 s)]
 alpha = 2.3e4    # coeficiente de absorcion [1/cm]
@@ -62,9 +64,10 @@ def gfcn(x):
 
 sys.generation(gfcn)
 
+#####################################################################################################
 #Generacion de voltajes iniciales bias
 voltages = np.linspace(0, 0.95, 40) #Genera los voltajes
-j = sesame.IVcurve(sys, voltages, '1dhomo_V') #Genera las corrientes
+j = sesame.IVcurve(sys, voltages, '1dhomo_V') #Guarda los datos de las corriente adimensionales
 j = j * sys.scaling.current #Da unidades de corriente en voltios
 
 #Graficos
